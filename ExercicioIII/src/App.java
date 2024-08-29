e80634a1e9c0959d6f3e2e2fe99ed75c8dcaeb82
Arthur Kramberger <arthur1curi@gmail.com>
2024-08-29 20:56:58 -0300

    Exercicio III

--- ExercicioIII/src/App.java
+++ ExercicioIII/src/App.java
@@ -1,5 +1,33 @@
+/*
+Escreva uma função que retorne todos os subconjuntos de um conjunto de números. 
+Por exemplo, se a entrada for [1, 2], a saída deve ser [[], [1], [2], [1, 2]]. 
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] vect = {1, 2};
+
+        List<List<Integer>> list = new ArrayList<>();
+        list = subconjuntos(vect);
+        System.out.println(list);
+    }
+
+    public static List<List<Integer>> subconjuntos(int[] vect){
+        List<List<Integer>> subconjuntos = new ArrayList<>();
+        
+
+        for (int num : vect) {
+            List<List<Integer>> novosSubconjuntos = new ArrayList<>();
+            for (List<Integer> subconjunto : subconjuntos) {
+                List<Integer> novoSubconjunto = new ArrayList<>(subconjunto);
+                novoSubconjunto.add(num);
+                novosSubconjuntos.add(novoSubconjunto);
+            }
+        subconjuntos.addAll(novosSubconjuntos);
+        }
+    return subconjuntos;
     }
 }
